4d591313808ad292c5a8ce7b79361bf301f4b65b
Gerard Talla <gerard.tchano@gmail.com>
2024-06-18 16:32:49 +0200

    Initial commit

---
+++ src/test/java/com/tallacoding/graphql_spring/GraphqlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallacoding.graphql_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
